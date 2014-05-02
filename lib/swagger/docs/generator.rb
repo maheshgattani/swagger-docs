cc5a8030aad2265aaa5cfb6bf7a602040f441b18
Mahesh Gattani <mahesh.gattani@birchbox.com>
2014-05-02 13:25:18 -0400

    doing a to string

--- lib/swagger/docs/generator.rb
+++ lib/swagger/docs/generator.rb
@@ -90,7 +90,7 @@ module Swagger
 
         def camelize_keys_deep!(h)
           h.keys.each do |k|
-            unless k == "properties"
+            unless k.to_s == "properties"
               ks    = k.to_s.camelize(:lower)
               h[ks] = h.delete k
               camelize_keys_deep! h[ks] if h[ks].kind_of? Hash
